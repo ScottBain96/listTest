3ccee7b794fbe19e419b938b7921d38fa750e12e
Scott PC UK <40214330@live.napier.ac.uk>
2018-03-17 19:49:26 +0000

    Testing ListView
    Test

---
+++ WpfApp3/WpfApp3/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    class Class1
+    {
+      
+        public string GpoName { get; set; }
+        public string GpoDescription { get; set; }
+        public string GpoValue { get; set; }
+
+
+        public Class1(string gpoName, string gpoDescription, string gpoValue)
+        {
+            GpoName = gpoName;
+            GpoDescription = gpoDescription;
+            GpoValue = gpoValue;
+
+
+        }
+    
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
